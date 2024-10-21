74ee2f3739a393548bed2626ff4d0bfb949064b6
Griffin <omondigriffin4@gmail.com>
2024-10-21 17:55:45 +0200

    registers the category model on the admin panel

--- Resources/urls.py
+++ Resources/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VideoUploadView, VideoListView
+from .views import VideoUploadView, VideoListView, CategoriesView
 
 urlpatterns = [
     path('upload/', VideoUploadView.as_view(), name='video-upload'),
     path('videos/', VideoListView.as_view(), name='video-list'),
+    path('categories/', CategoriesView.as_view(), name = 'Categories'),
 ]
 
