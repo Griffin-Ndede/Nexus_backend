484c5c35261ff34f8d9185f18f95bdccb3020ac4
Griffin <omondigriffin4@gmail.com>
2024-10-25 08:52:53 +0200

    creates modifications to the admin panel

--- Resources/urls.py
+++ Resources/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
-from .views import VideoUploadView, VideoListView, CategoriesView
+from .views import (
+    Form1VideoUploadView, Form1VideoListView,
+    Form2VideoUploadView, Form2VideoListView,
+    Form3VideoUploadView, Form3VideoListView,
+    Form4VideoUploadView, Form4VideoListView,
+    CategoriesView
+)
 
 urlpatterns = [
-    path('upload/', VideoUploadView.as_view(), name='video-upload'),
-    path('videos/', VideoListView.as_view(), name='video-list'),
-    path('categories/', CategoriesView.as_view(), name = 'Categories'),
-]
+    # Form 1 Video URLs
+    path('upload/form1/', Form1VideoUploadView.as_view(), name='form1-video-upload'),
+    path('videos/form1/', Form1VideoListView.as_view(), name='form1-video-list'),
 
+    # Form 2 Video URLs
+    path('upload/form2/', Form2VideoUploadView.as_view(), name='form2-video-upload'),
+    path('videos/form2/', Form2VideoListView.as_view(), name='form2-video-list'),
+
+    # Form 3 Video URLs
+    path('upload/form3/', Form3VideoUploadView.as_view(), name='form3-video-upload'),
+    path('videos/form3/', Form3VideoListView.as_view(), name='form3-video-list'),
+
+    # Form 4 Video URLs
+    path('upload/form4/', Form4VideoUploadView.as_view(), name='form4-video-upload'),
+    path('videos/form4/', Form4VideoListView.as_view(), name='form4-video-list'),
+
+    # Categories
+    path('categories/', CategoriesView.as_view(), name='categories'),
+]
