729fbc9617dc234c19bbcdabff321ab2c258db76
Griffin <omondigriffin4@gmail.com>
2024-10-27 12:56:01 +0200

    initiates deployment on render

--- Nexus_backend/settings.py
+++ Nexus_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,11 +87,16 @@ WSGI_APPLICATION = 'Nexus_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        "postgresql://nexus_db_d7q6_user:AbflVTpaB9L3PjOvm0aQOABJQgS8TWj6@dpg-csf1ce68ii6s739aae00-a.oregon-postgres.render.com/nexus_db_d7q6"
+    )
 }
 
 
